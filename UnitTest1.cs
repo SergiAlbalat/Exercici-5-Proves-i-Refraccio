7549391fdc8936c19abc435833bfa010f17d0aa4
SergiAlbalat <sergi.albalat.7e8@itb.cat>
2024-12-01 18:39:24 +0100

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,13 @@
+using ExerciseFive;
+namespace Exercici_5_Proves_i_Refraccio
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void validateAgeTest1()
+        {
+            int age = -1;
+            int result = 
+        }
+    }
+}
